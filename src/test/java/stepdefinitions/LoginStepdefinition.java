f7b42b64954594d2e34e62e400c5eb34ecb496db
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2025-07-25 05:01:20 +0300

    Furkan V3

--- src/test/java/stepdefinitions/LoginStepdefinition.java
+++ src/test/java/stepdefinitions/LoginStepdefinition.java
@@ -6,14 +6,22 @@ import io.cucumber.java.en.When;
 public class LoginStepdefinition {
     LoginPage login = new LoginPage();
 
-    //US08
     @When("As a user, I want to use the Use email instead option to log in with my email and password.")
     public void switchToEmailLogin() {
         login.emailInsteadClick();
     }
 
+    @When("As a Manager, I want to use the Use email instead option to log in with my email and password.")
+    public void switchToManagerMailLogin() {
+        login.emailInsteadClick();
+    }
+
     @When("As a user muss be {string} mail and {string} password Login")
     public void as_a_user_muss_be_mail_and_password_login(String mailAdress, String password) {
+        login.mailLogin(mailAdress, password);
+    }
+    @When("As a manager muss be {string} mail and {string} password Login")
+    public void as_a_manager_muss_be_mail_and_password_login(String mailAdress, String password) {
         login.mailLogin(mailAdress, password);
     }
 }
