16a0040df95296699104b4ca73a3d99610d6341c
TugbaKaragulle <karagulletugba@gmail.com>
2025-07-27 00:44:30 +0200

    US_10_Done

--- src/test/java/Page/TugbaPage.java
+++ src/test/java/Page/TugbaPage.java
@@ -1,4 +1,106 @@
 package Page;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import lombok.Getter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
+import java.util.*;
+
+import static utilities.Driver.getAppiumDriver;
+
+@Getter
 public class TugbaPage {
+
+    public TugbaPage() {
+        PageFactory.initElements(new AppiumFieldDecorator(getAppiumDriver()), this);
+    }
+    public static AndroidDriver driver = (AndroidDriver) getAppiumDriver();
+
+    //US_010 icin locate ve methodlar
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Most Popular\")")
+    private WebElement mostPopularTitle;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().description(\"See All\")")
+    private WebElement seeAllButton;
+
+    @AndroidFindBy(xpath = "//android.view.View[contains(@content-desc, 'Reviews') and not(contains(@content-desc, 'Most Popular')) and not(contains(@content-desc, 'Products Found'))]")
+    private List<WebElement> productsList;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(0)")
+    private WebElement backButton;
+
+    private By lastProduct = AppiumBy.androidUIAutomator("new UiSelector().description(\"The Children's Place Baby Girls' and Toddler Short Sleeve Everyday Dresses\n" +
+            "0 (0  Reviews)\n" +
+            "$29.00\")");
+
+    @AndroidFindBy(xpath = "//android.view.View[contains(@content-desc, 'Products Found')]")
+    private  WebElement productsFoundText;
+
+    public boolean isMostPopularTitleDisplayed(){
+        return mostPopularTitle.isDisplayed();
+    }
+
+    public void swipeUp() {
+        final var finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        var start = new Point(584, 1705);
+        var end = new Point(596, 906);
+        var swipe = new Sequence(finger, 1);
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(0),
+                PointerInput.Origin.viewport(), start.getX(), start.getY()));
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(500),
+                PointerInput.Origin.viewport(), end.getX(), end.getY()));
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+        driver.perform(Arrays.asList(swipe));
+
+    }
+
+    //Bu method elementi bulana kadar swipe yapar
+    public void swipeUpTo(By by) {
+        List<WebElement> elementList = driver.findElements(by); //Burada sadece bir elementin locate'i var, excp. atmasin diye liste atiyoruz
+        while (elementList.isEmpty()) { //o element liste eklenmedikce while döngüsü devam etsin
+            swipeUp();
+            elementList = driver.findElements(by); //elementi ekleyince while'dan cikacak ve swipe bitecek
+        }
+
+    }
+
+    //Son ürüne kadar swipe yaparak bütün ürünleri benzersiz olarak sete ekleyen method
+    public boolean allProductSize() {
+        Set<String> uniqueProducts = new HashSet<>();
+        while (true) {
+            List<WebElement> visibleProducts = driver.findElements(
+                    AppiumBy.xpath("//android.view.View[contains(@content-desc, 'Reviews') and not(contains(@content-desc, 'Most Popular')) and not(contains(@content-desc, 'Products Found'))]")
+            );
+
+            for (WebElement product: visibleProducts){
+                uniqueProducts.add(product.getAttribute("content-desc"));
+            }
+
+            if (!driver.findElements(lastProduct).isEmpty()) {
+                break;
+            }
+            swipeUp();
+        }
+        // Ürün sayısı kontrolü
+        String text = productsFoundText.getAttribute("content-desc");
+        text = text.replaceAll("\\D", "");
+
+        return Integer.parseInt(text) == uniqueProducts.size();
+    }
+
+
+
+
+
 }
--- src/test/resources/features/US010_MostPopular.feature
+++ src/test/resources/features/US010_MostPopular.feature
@@ -2,12 +2,24 @@ Feature: US010 Most Popular
   As a user, I would like to have a page where I can follow the latest products on the site where I shop.
 
   Background:
-    Given User makes driver adjustments
-    Given User confirms to be on the homepage
+    * User makes driver adjustments
+    * User confirms to be on the homepage
 
-  @deneme
-  Scenario: Ana sayfada profile linki görünür olmalı ve Sign In sayfasına yönlendirmeli
-      Given User verifies the button with description "Profile"
+  @TugbaLogin
+  Scenario: Login olarak siteye giris
       When User clicks the button with description "Profile"
-      And User verifies the button with description "Sign In"
       And User clicks the button with description "Sign In"
+      And As a user, I want to use the Use email instead option to log in with my email and password.
+      And As a user muss be "tugbaCustomerMail" mail and "customerpassword" password Login
+
+    @TC_010.01
+Scenario:
+      * The user verifies that the Most Popular title is visible on the homepage
+      * The user verifies that the See All button is visible and enabled
+      * List Current products under Most Popular
+      * Click the back button
+      * User verifies the button with description "Home"
+
+  @TC_010.02
+  Scenario:
+    When
