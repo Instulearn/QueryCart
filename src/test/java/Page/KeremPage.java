5a318f9867b5f9f8f4e4ac32ceb9dae82cb224f3
Kerem Ceylan <keremceylan14@gmail.com>
2025-07-29 18:17:14 +0300

    US023_KC

---
+++ src/test/java/Page/KeremPage.java
@@ -0,0 +1,91 @@
+package Page;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import lombok.Getter;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import utilities.ReusableMethods;
+
+
+import static utilities.Driver.getAppiumDriver;
+
+@Getter
+public class KeremPage {
+
+    public KeremPage() {
+        PageFactory.initElements(new AppiumFieldDecorator(getAppiumDriver()), this);
+    }
+
+    public static AndroidDriver driver = (AndroidDriver) getAppiumDriver();
+
+    // Anasayfa Locator
+    @AndroidFindBy(xpath = "(//*[@class='android.widget.ImageView'])[3]")
+    private WebElement homepageBirinciUrunFavoriButonu;
+
+    @AndroidFindBy(xpath = "(//*[@class='android.widget.ImageView'])[10]")
+    private WebElement homepageIkinciUrunFavoriButonu;
+
+    @AndroidFindBy(xpath = "(//*[@class='android.widget.ImageView'])[2]")
+    private WebElement homepageMostPopularTitle;
+
+    @AndroidFindBy(xpath = "(//*[@class='android.widget.ImageView'])[1]")
+    private WebElement homePageQueryCartText;
+
+
+
+    //Sing Up Page Locator'lar
+    @AndroidFindBy(xpath = "(//*[@class='android.widget.EditText'])[1]")
+    private WebElement signupNameTextBox;
+
+    @AndroidFindBy(xpath = "(//*[@class='android.widget.EditText'])[3]")
+    private WebElement signupPasswordTextBox;
+
+    @AndroidFindBy(xpath = "(//*[@class='android.widget.EditText'])[2]")
+    private WebElement signupEmailTextBox;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Sign Up\").instance(1)")
+    private WebElement singupButton;
+
+
+
+    //Sing In Page Locator'lar
+    @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Sign In\").instance(0)")
+    private WebElement signinText;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Sign In\").instance(1)")
+    private WebElement signinButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.EditText\").instance(0)")
+    private WebElement signinEmailTextBox;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.EditText\").instance(1)")
+    private WebElement signinPasswordTextBox;
+
+
+
+
+
+
+
+    // METHODLAR
+    public void clickByDescription (String description) {
+        AndroidDriver driver = (AndroidDriver) getAppiumDriver();
+        WebElement button = driver.findElement(AppiumBy.androidUIAutomator(
+                "new UiSelector().description(\"" + description + "\")"));
+        button.click();
+        ReusableMethods.wait(3);
+
+    }
+}
+
+
+
+
+
+
+
+
+
