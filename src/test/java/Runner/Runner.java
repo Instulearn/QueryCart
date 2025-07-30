a3c6e9206c38c15e8135ba7ca74fdf1b81ae1eab
Canan Özgürsoy <canansirmaa@gmail.com>
2025-07-30 15:15:37 -0400

    update

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@test22",
+        tags = "@browser",
         dryRun = false
 
 
